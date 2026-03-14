3b4a0cf440e6726f8c06bcaed0c5de9edf864143
Raymond Oyondi <raymondoyondi@gmail.com>
2026-03-14 14:33:25 -0500

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,54 @@
+export const sidebarLinks = [
+    {
+      imgURL: '/icons/Home.svg',
+      route: '/',
+      label: 'Home',
+    },
+  
+    {
+      imgURL: '/icons/upcoming.svg',
+      route: '/upcoming',
+      label: 'Upcoming',
+    },
+    {
+      imgURL: '/icons/previous.svg',
+      route: '/previous',
+      label: 'Previous',
+    },
+    {
+      imgURL: '/icons/recordings.svg',
+      route: '/recordings',
+      label: 'Recordings',
+    },
+    {
+      imgURL: '/icons/add-personal.svg',
+      route: '/personal-room',
+      label: 'Personal Room',
+    },
+  ];
+  
+  export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+  ];
+
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/icons/github.svg",
+      link: "https://github.com/raymondoyondi"
+    },
+    {
+      id: 2,
+      img: "/icons/twit.svg",
+      link: "https://x.com/raymondoyondi"
+    },
+    {
+      id: 3,
+      img: "/icons/link.svg",
+      link: "https://www.linkedin.com/in/raymond-oyondi/"
+    },
+  ];
